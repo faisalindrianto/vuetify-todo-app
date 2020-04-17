829884bbb4855cb747cc8ad812666cf5c7a0e745
faisalindrianto <wwf051099@gmail.com>
2020-04-17 23:14:23 +0700

    buttons & icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore omnis laborum animi cumque delectus? Doloremque libero unde nesciunt culpa, fuga facere veritatis illum blanditiis? Exercitationem incidunt natus consectetur pariatur ea.</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, reprehenderit distinctio dolorum voluptatem aut eligendi tenetur eveniet amet, quos natus dignissimos sed maxime quam similique, nostrum esse inventore ab itaque.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Small display</h4>
-    <p class="headline">This is a headline.</p>
-    <p class="subheading font-weight-bold">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink" dark class="ml-2">click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
   </div>
 </template>
 
