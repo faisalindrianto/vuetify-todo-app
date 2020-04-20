c6f007f728abf89f73f5987b7282f9d51acc5b7b
faisalindrianto <wwf051099@gmail.com>
2020-04-21 03:42:58 +0700

    adding navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <v-toolbar flat app>
-      <v-toolbar-title class="text-uppercase grey--text">
+      <v-btn color="pink" dark @click.stop="drawer = !drawer">
+        Toggle
+      </v-btn>
+      <v-toolbar-title class="text-uppercase grey--text ml-2">
         <span class="font-weight-light">My</span>
         <span>Todo</span>
       </v-toolbar-title>
@@ -11,11 +14,26 @@
         <v-icon right>mdi-exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {
-  
-}
+  export default {
+    data() {
+      return {
+        drawer: null,
+        items: [{
+            title: 'Home'
+          },
+          {
+            title: 'About'
+          },
+        ],
+      }
+    },
+  }
 </script>
