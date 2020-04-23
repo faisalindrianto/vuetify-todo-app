5e03f982b1d2c42f9164644bf8bc8eace7654f78
faisalindrianto <wwf051099@gmail.com>
2020-04-23 22:18:50 +0700

    adding grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,26 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      content
+      <v-card outlined class="pa-3 pl-5">
+        <v-layout row class="ml-1">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Project Title</div>
+            <div>Faisal Indrianto</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due By</div>
+            <div>1st Jan 2020</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>Ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
