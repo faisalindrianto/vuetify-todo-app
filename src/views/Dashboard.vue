409687962bbbf90320af4ac49321d5be69d0da1d
faisalindrianto <wwf051099@gmail.com>
2020-04-24 19:01:01 +0700

    adding sort functionality

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="dashboard">
-    <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+    <h1 class="grey--text mb-4">Dashboard</h1>
+      <v-layout row class="mb-3 ml-1">
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small depressed class="mr-2" @click="sortBy('title')" v-on="on">
+              <v-icon left>folder</v-icon>
+              <span class="caption text-lowercase">By project name</span>
+            </v-btn>
+          </template>
+          <span>sort by project name</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small depressed @click="sortBy('person')" v-on="on">
+              <v-icon left>person</v-icon>
+              <span class="caption text-lowercase">By person</span>
+            </v-btn>
+          </template>
+          <span>sort by person name</span>
+        </v-tooltip>
+      </v-layout>
+
       <v-card outlined class="pa-3 pl-5" v-for="project in projects" :key="project.title">
         <v-layout row :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -18,8 +40,9 @@
             <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>{{ project.status }}</div>
+            <div>
+              <v-chip right small :class="`${project.status} white--text`">{{ project.status }}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
       </v-card>
@@ -38,6 +61,11 @@ export default {
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
     }
+  },
+  methods: {
+    sortBy(prop){
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+    }
   }
 }
 </script>
@@ -47,9 +75,18 @@ export default {
   border-left: 4px solid #3CD1C2;
 }
 .project.ongoing{
-  border-left: 4px solid orange
+  border-left: 4px solid orange;
 }
 .project.overdue{
   border-left: 4px solid tomato;
+}
+.v-chip.complete{
+  background: #3CD1C2 !important;
+}
+.v-chip.ongoing{
+  background: orange !important;
+}
+.v-chip.overdue{
+  background: tomato !important;
 }
 </style>
