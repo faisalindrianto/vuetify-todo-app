d238bc96d8632e561804ab595e0434c2b3420083
faisalindrianto <wwf051099@gmail.com>
2020-04-27 03:24:08 +0700

    adding popup component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,6 +38,10 @@
           </v-avatar>
           <p class="white--text subtitle-1">Indrianto</p>
         </v-flex>
+
+        <v-flex>
+          <Popup />
+        </v-flex>
       </v-layout>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -54,7 +58,11 @@
 </template>
 
 <script>
+import Popup from './Popup'
   export default {
+    components: {
+      Popup
+    },
     data() {
       return {
         drawer: null,
---
+++ src/components/Popup.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="white" v-on="on">Add New Project</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add New Project</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form class="px-3 py-2" ref="form">
+            <v-text-field label="title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+            <v-textarea label="information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
+            <v-menu
+              v-model="menu2"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="due"
+                  label="Due Date"
+                  prepend-icon="event"
+                  readonly
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="due" @input="menu2 = false"></v-date-picker>
+            </v-menu>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="primary" text @click="submit" class="mr-3">Add Project</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        dialog: false,
+        title: '',
+        content: '',
+        date: new Date().toISOString().substr(0, 10),
+        menu2: false,
+        inputRules: [
+          v => v.length >= 3 || 'Minimal 3 karakter slur'
+        ]
+      }
+    },
+    methods: {
+      submit(){
+       if(this.$refs.form.validate()){
+         console.log(this.title, this.content)
+       }
+      }
+    }
+  }
+</script>
